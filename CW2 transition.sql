6f0abad34cd7233828d4a445ceab393d58fc88da
Sabya <sdash0113@gmail.com>
2026-01-07 22:21:09 +0530

    practicing 2

--- CW2 transition.sql
+++ CW2 transition.sql
@@ -1,8 +1,26 @@
 use db_sabya;
 show tables;
+select * from orders;
 -- 1. Find the total number of orders.
 select count(*) as total_orders
 from orders;
+
+select sum(quantity) as total_quantity
+from orders;
+
+select avg(price) as avg_price
+from orders;
+
+select max(price) as max_price 
+from orders;
+
+select min(price) as max_price 
+from orders;
+
+
+
+
+
 
 
 
