e589bd74d5a55bc7a9a9708697d6c24cd43a4da3
Sabya <sdash0113@gmail.com>
2026-01-16 11:36:18 +0530

    config done

--- join practice3.sql
+++ join practice3.sql
@@ -53,4 +53,9 @@ inner join orders o
 on c.customer_id = o.customer_id
 where customer_name = "Rahul";
 
+-- 3
+select c.customer_name , c.city , o.order_amount
+from customers c
+left join orders o
+on c.customer_id = o.customer_id;
 
