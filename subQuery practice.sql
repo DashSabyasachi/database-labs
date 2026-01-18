5cb4e985a26c1484a93ad55dbacf8f14c1976a9c
Sabya <sdash0113@gmail.com>
2026-01-18 07:39:46 +0530

    config done

--- subQuery practice.sql
+++ subQuery practice.sql
@@ -2,15 +2,13 @@ use db_sabya;
 show tables;
 select * from employees;
 
-select emp_name, avg(salary) as avg_sal from employees;
 
+-- find employee who have more then the average salary for all employess.
 select * 
 from employees
 where salary > (select avg(salary)
 				from employees);
-                
-				
-
+                				
 
 -- find employee with same department as Amit's department
 select * 
@@ -26,3 +24,44 @@ from employees
 where salary = (select max(salary) 
 			   from employees 
                where department = "HR");
+               
+-- Questions
+-- 1.Find employees who earn more than the average salary of all employees.
+-- 2.Find employees who earn less than the average salary.
+-- 3.Find employees who earn the maximum salary.
+-- 4.Find employees who earn the minimum salary.
+-- 5.Find the second highest salary.
+-- 6.Find employees who earn more than Amit.
+-- 7.Find employees who work in the same department as Rina.
+-- 8.Find employees whose salary is equal to the highest salary in IT department.
+-- 9.Find employees who earn less than the lowest salary in HR.
+-- 10.Find employees who work in departments located in Mumbai.
+-- 11.Find employees whose salary is greater than ANY salary in HR department.
+-- 12.Find employees whose salary is greater than ALL salaries in IT department.
+-- 13.Find employees who belong to departments having more than one employee.
+-- 14.Find employees who earn more than the average salary of their department.
+-- 15.Find employees who earn the highest salary in their department.
+-- 16. Find employees who earn the lowest salary in their department.
+-- 17. Find employees who work in departments having at least one employee earning more than 70,000.
+-- 18. Find employees who belong to departments that exist.
+-- 19. Find employees who do not belong to any department.
+-- 20. Find departments where no employee earns more than 60,000.
+-- 21. Find employees whose salary is equal to the average salary of their department.
+-- 22. Find employees who work in departments where average salary is greater than overall average salary.
+-- 23. Find employees who share the same salary with someone else.
+-- 24. Find employees who earn exactly the same salary as at least one other employee.
+-- 25. Find employees who earn more than all employees in Admin department.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
