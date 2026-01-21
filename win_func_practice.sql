24dbf1dec1573d8c791827fdbb0f298f8a51bec7
Sabya <sdash0113@gmail.com>
2026-01-21 12:15:39 +0530

    config done

--- win_func_practice.sql
+++ win_func_practice.sql
@@ -30,31 +30,56 @@ select *, count(subject)
 from students
 group by subject;
 
--- row_number()
+-- ROW_NUMBER()
 select student_name, marks, subject, 
 	row_number() over(partition by subject order by marks desc) as rownum
 from students;
 
 
--- rank()
+-- RANK()
 select student_name , marks, subject,
    rank() over(partition by subject order by marks desc) as rnk
 from students;
 
 
 
--- dense_rank()
+-- DENSE_RANK()
 select student_name, marks, subject,
 	dense_rank() over(partition by subject order by marks desc)as dnsrnk
 from students;
 
--- count(), sum() , avg(), max(), min()
+-- COUNT(), SUM() , AVG(), MAX(), MIN()
 select student_name, marks, subject,
-avg(marks) over(partition by subject ) as avg_marks
+	avg(marks) over(partition by subject ) as avg_marks
 from students;
 
 select student_name, marks, subject,
-count(student_id) over(partition by subject ) as avg_marks
+	count(student_id) over(partition by subject ) as avg_marks
 from students;
+
+select student_name, marks, subject,
+	sum(marks) over(partition by subject ) as total_marks
+from students;
+
+select student_name, marks, subject,
+	max(marks) over(partition by subject) as total_marks
+from students;
+	
+select student_name, marks, subject,
+	min(marks) over(partition by subject) as total_marks
+from students;
+
+
+-- LEAD()
+select student_name, subject, marks,
+	lead(marks) over(partition by subject order by marks desc) as next_row_marks
+from students;
+
+
+-- LAG()
+select student_name, subject , marks,
+	lag(marks) over(partition by subject order by marks desc) as previous_row_marks
+from students;
+
 
 
